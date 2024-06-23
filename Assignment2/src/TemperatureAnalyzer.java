f1cf460404436c657dbb5d3003e31756eedd9881
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-06-22 22:39:47 -0400

    Assignment 2 - Commit 1 - Add header (Correct File)

---
+++ Assignment2/src/TemperatureAnalyzer.java
@@ -0,0 +1,11 @@
+/*
+TemperatureAnalyzer.java
+Date: 22nd June, 2024
+Name: Mohammed Aasim Ali Khan
+Description: This program analyzes temperature data for multiple days, calculates averages, and identifies the days with the lowest and highest temperatures.
+*/
+
+import java.util.Scanner;
+
+public class TemperatureAnalyzer {
+}
