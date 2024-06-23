d0f166c73364d66d090d2a3377e56c207704d8a3
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-06-22 22:44:28 -0400

    Assignment 2 - Commit 2 - Add Input Validation

--- Assignment2/src/TemperatureAnalyzer.java
+++ Assignment2/src/TemperatureAnalyzer.java
@@ -8,4 +8,23 @@ Description: This program analyzes temperature data for multiple days, calculate
 import java.util.Scanner;
 
 public class TemperatureAnalyzer {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final int MIN_DAYS = 1;
+        final int MAX_DAYS = 31;
+        final int MIN_TEMP = -30;
+        final int MAX_TEMP = 30;
+        int numDays;
+
+        // Get the number of days from the user
+        do {
+            System.out.print("Enter the number of days (1-31): ");
+            numDays = input.nextInt();
+        } while (numDays < MIN_DAYS || numDays > MAX_DAYS);
+
+        double[] lowTemps = new double[numDays];
+        double[] highTemps = new double[numDays];
+
+
+    }
 }
