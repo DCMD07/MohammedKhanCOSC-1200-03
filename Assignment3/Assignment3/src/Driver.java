eb41b0893978c485fdab08176239e70a3e4526a0
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-07-29 00:22:11 -0400

    Assignment 3: Commit 4: Displayed details of the default vaccine

--- Assignment3/Assignment3/src/Driver.java
+++ Assignment3/Assignment3/src/Driver.java
@@ -11,3 +11,7 @@ public class Driver {
         // Create an array to save all the vaccines
         Vaccine[] vaccines = new Vaccine[3];
 
+        // Default vaccine
+        vaccines[0] = new Vaccine();
+        System.out.println("Displaying default vaccine details in a non-linear format:");
+        vaccines[0].display(false);
