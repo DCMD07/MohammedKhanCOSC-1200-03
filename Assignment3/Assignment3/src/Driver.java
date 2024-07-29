2f12cbd1097cf60f09af117f45412a4664143513
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-07-29 00:21:14 -0400

    Assignment 3 - Commit3:  Initialized an array to store multiple Vaccine objects

---
+++ Assignment3/Assignment3/src/Driver.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Mohammed Aasim
+ * Date: July 26, 2024
+ * Description: Creates vaccine instances and displays their details in different formats.
+ */
+
+import java.time.LocalDate;
+
+public class Driver {
+    public static void main(String[] args) {
+        // Create an array to save all the vaccines
+        Vaccine[] vaccines = new Vaccine[3];
+
