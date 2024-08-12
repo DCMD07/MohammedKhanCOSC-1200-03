041ecf8f4192a760841c98477c2ec9fb1e04a83d
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-08-12 19:42:19 -0400

    Assignment-4 Commit-4 - Created perishable product examples in the Driver class

--- Assignment4/ProductManagementSystem/src/Driver.java
+++ Assignment4/ProductManagementSystem/src/Driver.java
@@ -7,3 +7,7 @@ public class Driver {
         Product p1 = new Product(101, "Laptop", 999.99, 10, 5, "Handle with care");
         Product p2 = new Product(102, "Smartphone", 599.99, 20, 10, "Fragile");
         Product p3 = new Product(103, "Tablet", 299.99, 15, 8, "Keep dry");
+
+        // Create instances of PerishableProduct with sample data
+        PerishableProduct pp1 = new PerishableProduct(201, "Milk", 2.99, 100, 50, "Keep refrigerated", new Date(2024, 8, 15));
+        PerishableProduct pp2 = new PerishableProduct(202, "Cheese", 5.99, 50, 20, "Store in a cool place", new Date(2024, 9, 10));
