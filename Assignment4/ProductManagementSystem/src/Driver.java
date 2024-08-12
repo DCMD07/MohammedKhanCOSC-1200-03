283f5fb141f19650139f6d704177016eebaec1a1
DCMD07 <mohammedaasimali.khan@dcmail.ca>
2024-08-12 19:39:47 -0400

    Assignment-4 Commit-3 - Created product examples in the Driver class

---
+++ Assignment4/ProductManagementSystem/src/Driver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Driver {
+    public static void main(String[] args) {
+        // Create instances of Product with sample data
+        Product p1 = new Product(101, "Laptop", 999.99, 10, 5, "Handle with care");
+        Product p2 = new Product(102, "Smartphone", 599.99, 20, 10, "Fragile");
+        Product p3 = new Product(103, "Tablet", 299.99, 15, 8, "Keep dry");
